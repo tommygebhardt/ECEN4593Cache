95c83c64ef1cc043522f45f13d9a4ea37442bb19
Tommy Gebhardt <tommygebhardt@gmail.com>
2015-04-18 12:05:33 -0600

    Further refinements on cache.h. Started adding global stats struct

--- cache.h
+++ cache.h
@@ -1,6 +1,24 @@
 #ifndef CACHE_H
 #define CACHE_H
+struct stats{
+  unsigned long long int exec_time;
+  unsigned long long int flush_time;
 
+  unsigned long long int L1I_hit_count;
+  unsigned long long int L1D_hit_count;
+  unsigned long long int L1I_miss_count;
+  unsigned long long int L1D_miss_count;
+  unsigned long long int L2_hit_count;
+  unsigned long long int L2_miss_count;
+  
+  unsigned long long int kickouts;
+  unsigned long long int dirty_kickouts;
+
+  unsigned long long int inst_count;
+  unsigned long long int read_count;
+  unsigned long long int write_count;
+  unsigned long long int total_count;
+};
 struct block{
   bool dirty;
   bool valid;
@@ -23,19 +41,28 @@ struct set{
   set(unsigned int ways){
     blocks = new block[assoc];
     assoc = ways;
+    // TODO: Initialize LRU HERE
   }
-}
+};
 
 class cache{
  private:
   unsigned int cache_size;
   unsigned int assoc;
   unsigned int block_size;
-  unsigned int hit_time;
-  unsigned int miss_time;
-  unsigned int transfer_time;
-  unsigned int bus_width;
-  
+  int hit_time;
+  int miss_time;
+  int transfer_time;
+  int bus_width;
+  set * table;
+
+ public:
+  cache();
+  cache(char * filename);
+  cache(unsigned int cache_size, unsigned int assoc, unsigned int block_size, int hit_time, int miss_time, int transfer_time, int bus_width);
+  bool read(unsigned long long int address, unsigned int bytesize);
+  bool write(unsigned long long int address, unsigned int bytesize);
+  void flush();
 }
 
 
