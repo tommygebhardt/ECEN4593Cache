a2aa6340a042092d2621c6ac65f145519e957636
Conrad <conrad.hougen@gmail.com>
2015-04-19 01:02:18 -0600

    Pushing main.cpp changes from earlier Saturday

--- main.cpp
+++ main.cpp
@@ -25,93 +25,95 @@ int main(int argc, char ** argv)
     char op;
     unsigned long long int address;
     unsigned int bytesize;
+    // Flush counter
+    unsigned int flush_counter = 0;
 
     // Take command line input
     if (argc == 2) {
-    string line;
-    ifstream file(argv[1]);
-    size_t position;
-    getline(file, line);
-    // Go to end of comments
-    while(line[0] == '#'){
-    getline(file, line);
-    }
-    // First line is L1 block size
-    position = line.find(',');
-    bsize = stoi(line.substr(position+1));
+        string line;
+        ifstream file(argv[1]);
+        size_t position;
+        getline(file, line);
+        // Go to end of comments
+        while(line[0] == '#'){
+            getline(file, line);
+        }
+        // First line is L1 block size
+        position = line.find(',');
+        bsize = stoi(line.substr(position+1));
 
-    // Second line is L1 cache size
-    getline(file,line);
-    position = line.find(',');
-    csize = stoi(line.substr(position+1));
+        // Second line is L1 cache size
+        getline(file,line);
+        position = line.find(',');
+        csize = stoi(line.substr(position+1));
 
-    // Third line is L1 associativity
-    getline(file,line);
-    position = line.find(',');
-    ways = stoi(line.substr(position+1));
+        // Third line is L1 associativity
+        getline(file,line);
+        position = line.find(',');
+        ways = stoi(line.substr(position+1));
 
-    // Fourth line is L1 hit time
-    getline(file,line);
-    position = line.find(',');
-    htime = stoi(line.substr(position+1));
+        // Fourth line is L1 hit time
+        getline(file,line);
+        position = line.find(',');
+        htime = stoi(line.substr(position+1));
 
-    // Fifth line is L1 miss time
-    getline(file,line);
-    position = line.find(',');
-    mtime = stoi(line.substr(position+1));
+        // Fifth line is L1 miss time
+        getline(file,line);
+        position = line.find(',');
+        mtime = stoi(line.substr(position+1));
 
-    // Sixth line is L2 block size
-    getline(file,line);
-    position = line.find(',');
-    bsizeL2 = stoi(line.substr(position+1));
+        // Sixth line is L2 block size
+        getline(file,line);
+        position = line.find(',');
+        bsizeL2 = stoi(line.substr(position+1));
 
-    // Seventh line is L2 cache size
-    getline(file,line);
-    position = line.find(',');
-    csizeL2 = stoi(line.substr(position+1));
+        // Seventh line is L2 cache size
+        getline(file,line);
+        position = line.find(',');
+        csizeL2 = stoi(line.substr(position+1));
 
-    // Eighth line is L2 associativity
-    getline(file,line);
-    position = line.find(',');
-    waysL2 = stoi(line.substr(position+1));
+        // Eighth line is L2 associativity
+        getline(file,line);
+        position = line.find(',');
+        waysL2 = stoi(line.substr(position+1));
 
-    // Ninth line is L2 hit time
-    getline(file,line);
-    position = line.find(',');
-    htimeL2 = stoi(line.substr(position+1));
+        // Ninth line is L2 hit time
+        getline(file,line);
+        position = line.find(',');
+        htimeL2 = stoi(line.substr(position+1));
 
-    // Tenth line is L2 miss time
-    getline(file,line);
-    position = line.find(',');
-    mtimeL2 = stoi(line.substr(position+1));
+        // Tenth line is L2 miss time
+        getline(file,line);
+        position = line.find(',');
+        mtimeL2 = stoi(line.substr(position+1));
 
-    // Eleventh line is L2 transfer time (put in L1 cache)
-    getline(file,line);
-    position = line.find(',');
-    trantime = stoi(line.substr(position+1));
+        // Eleventh line is L2 transfer time (put in L1 cache)
+        getline(file,line);
+        position = line.find(',');
+        trantime = stoi(line.substr(position+1));
 
-    // Twelveth line is L2 bus width (put in L1 cache)
-    getline(file,line);
-    position = line.find(',');
-    bwidth = stoi(line.substr(position+1));
+        // Twelveth line is L2 bus width (put in L1 cache)
+        getline(file,line);
+        position = line.find(',');
+        bwidth = stoi(line.substr(position+1));
 
     } else if (argc == 1) {
-    // Default values
-    csize = 8192;
-    bsize = 32;
-    ways = 1;
-    htime = 1;
-    mtime = 1;
-    trantime = 2; // L1 to L2
-    bwidth = 16; // L1 to L2
-    csizeL2 = 32768;
-    bsizeL2 = 64;
-    waysL2 = 1;
-    htimeL2 = 5;
-    mtimeL2 = 7;
+        // Default values
+        csize = 8192;
+        bsize = 32;
+        ways = 1;
+        htime = 1;
+        mtime = 1;
+        trantime = 2; // L1 to L2
+        bwidth = 16; // L1 to L2
+        csizeL2 = 32768;
+        bsizeL2 = 64;
+        waysL2 = 1;
+        htimeL2 = 5;
+        mtimeL2 = 7;
     } else {
-    cout << "Incorrect usage: Only zero or one command line argument accepted" << endl;
-    return -1;
+        cout << "Incorrect usage: Only zero or one command line argument accepted" << endl;
+        return -1;
     }
 
     trantimeL2 = mem_chunktime; // L2 to main memory 
@@ -134,12 +136,62 @@ int main(int argc, char ** argv)
     bytesize = 4;
     L1D.read(address, bytesize);
 #endif
-    /*
     // Main loop to read trace data and start tracking statistics
     while(scanf("%c %Lx %d\n", &op, &address, &bytesize) == 3)
     {
-
+        execution.total_count++; // Add to total number of references
+        
+        if(flush_counter == 380000)
+        {
+            // flush the caches
+        }
+        
+        switch(op)
+        {
+            case 'I':
+                execution.inst_count++;
+                // Read from L1 instruction cache
+                if(!L1I.read(address, bytesize))
+                {
+                    // Miss, so read from L2 unified cache
+                    if(!L2.read(address, bytesize))
+                    {
+                        // Miss, so access memory
+                        execution.exec_time += mem_sendaddr + mem_ready;
+                    }
+                }
+                break;
+            case 'R':
+                execution.read_count++;
+                // Read from L1 data cache
+                if(!L1D.read(address, bytesize))
+                {
+                    // Miss, so read from L2 unified cache
+                    if(!L2.read(address, bytesize))
+                    {
+                        // Miss, so access memory
+                        execution.exec_time += mem_sendaddr + mem_ready;
+                    }
+                }
+                break;
+            case 'W':
+                execution.write_count++;
+                // Write to L1 data cache
+                if(!L1D.write(address, bytesize))
+                {
+                    // Miss, so read from L2 unified cache
+                    if(!L2.read(address, bytesize))
+                    {
+                        // Miss, so access memory
+                        execution.exec_time += mem_sendaddr + mem_ready;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        
+        flush_counter++;
     }
-    */
     return 0;
 }
